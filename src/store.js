d5839e1983ee120ed0148afe54c3f72d109b70dd
zia-ur-rehman-khan <03112397853zia@gmail.com>
2021-07-11 08:56:48 +0500

    working completed

--- src/store.js
+++ src/store.js
@@ -2,9 +2,9 @@ import { createStore } from "redux";
 
 const initial = {
   user: "loading",
-  productsdetailed:'',
-  search : "",
-  cardUserId:""
+  productsdetailed: '',
+  search: "",
+  cardUserId: ""
 };
 
 const Reducer = (state = initial, action) => {
@@ -16,25 +16,25 @@ const Reducer = (state = initial, action) => {
       return { ...state, user: action.payload };
 
     case "username":
-      return { ...state, user: { ...state?.user, userName: action.payload } };
+      return { ...state, user: { ...state.user, userName: action.payload } };
 
     case "productsdetailed":
-        return {...state,productsdetailed:action.payload}
+      return { ...state, productsdetailed: action.payload }
     case "addProducts":
-        return {
-          ...state, productsdetailed: [...state?.productsdetailed, action.payload]
-        }
+      return {
+        ...state, productsdetailed: [...state.productsdetailed, action.payload]
+      }
     case "search":
-      return { ...state,search:action.payload}
-      case "delete":
-        var filter = state.productsdetailed.filter(item =>item._id !== action.payload
-        )
-        return{...state,productsdetailed:filter}
+      return { ...state, search: action.payload }
+    case "delete":
+      var filter = state.productsdetailed.filter(item => item._id !== action.payload
+      )
+      return { ...state, productsdetailed: filter }
 
     case "classUserId":
       console.log("hello", action.payload)
-      return{...state,cardUserId:action.payload}
-    default:    
+      return { ...state, cardUserId: action.payload }
+    default:
       return state;
   }
 };
